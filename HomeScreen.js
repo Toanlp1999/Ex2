e767f9b204056213e59715d7b8c4118b4dcabf68
Your Name <you@example.com>
2024-04-21 17:43:11 +0700

    first commit

---
+++ HomeScreen.js
@@ -0,0 +1,21 @@
+import { Button, Text } from 'react-native-paper' ;
+import {View, StyleSheet} from 'react-native'
+const HomeScreen= ({ navigation }) => {
+    return (
+    <View style={style.container}>
+    <Text>Home Screen</Text>
+    <Button mode ="contained" onPress={() => navigation.navigate('Details')}>
+        Go to details
+    </Button>
+    </View>
+    );
+}
+const style = StyleSheet.create({
+    container:{
+        flex: 1,
+        alignItems :'center',
+        justifyContent: 'center' ,
+    
+    }
+});
+export default HomeScreen;
